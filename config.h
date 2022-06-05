befb4bcc6e5ddbec79ef0d7f8907b19f91e4de76
Matheus Henrique <matheus.hpp135@gmail.com>
2022-06-05 03:07:42 -0300

    add new font support

--- config.h
+++ config.h
@@ -6,6 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font2 = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
